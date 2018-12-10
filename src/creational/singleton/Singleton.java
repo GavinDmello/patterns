94be7ae0d19118b9c96c28dfa7f8eb599a6fcb74
Gavin D'mello <dmellogavin5000@gmail.com>
2018-12-10 17:32:22 +0000

    Make object creation in a lazy manner

--- src/creational/singleton/Singleton.java
+++ src/creational/singleton/Singleton.java
@@ -5,11 +5,15 @@ package creational.singleton;
  * @author gavin
  */
 public class Singleton {
-    private static Singleton s = new Singleton();
+    private static Singleton s = null;
     
     private Singleton() {}
     
     public static Singleton getInstance() {
+        if (s == null) {
+            s = new Singleton();
+        }
+    
         return s;
     }
 }
